bb9222551e6de1a1a48bb52c0148cd039696091a
Gabriel Gavilan <ggavilan@redpoints.com>
2020-12-01 23:22:11 +0100

    Small improvements

--- solutions/day_1.py
+++ solutions/day_1.py
@@ -3,17 +3,16 @@ from typing import Set
 
 
 def solve(report: Set[int], parts: int, target: int) -> int:
-    for r_i in report:
-        if parts == 1 and r_i == target:
-            return r_i
-        if r_i > target:
+    for r in report:
+        if parts == 1 and r == target:
+            return r
+        if r > target:
             continue
-        if parts > 1:
-            answer = r_i * solve(
-                report - {r_i},
-                parts - 1,
-                target - r_i
-            )
-            if answer:
-                return answer
+        answer = r * solve(
+            report - {r},
+            parts - 1,
+            target - r
+        )
+        if answer:
+            return answer
     return 0
